06243e67a32e7348c52615426050097015fb9406
AndriiAndrey <semykinandrey@gmail.com>
2019-12-19 12:00:49 +0200

    Updates

---
+++ precache-manifest.0d2da746f21397bcd2ea3bb9d02b9033.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56da0e06b2dfe74386ffd23bb1a3c6d3",
+    "url": "/firs-react-app/index.html"
+  },
+  {
+    "revision": "d3254b0f789fcd7742b0",
+    "url": "/firs-react-app/static/css/3.a5b399f1.chunk.css"
+  },
+  {
+    "revision": "4acd8e7b44c5d763fdee",
+    "url": "/firs-react-app/static/css/4.8034b984.chunk.css"
+  },
+  {
+    "revision": "0b21b8b99b8c52dc6db9",
+    "url": "/firs-react-app/static/css/main.3f68c097.chunk.css"
+  },
+  {
+    "revision": "ff9e24e068c7da6067c3",
+    "url": "/firs-react-app/static/js/2.21b58663.chunk.js"
+  },
+  {
+    "revision": "d3254b0f789fcd7742b0",
+    "url": "/firs-react-app/static/js/3.82bc906f.chunk.js"
+  },
+  {
+    "revision": "4acd8e7b44c5d763fdee",
+    "url": "/firs-react-app/static/js/4.8d11addc.chunk.js"
+  },
+  {
+    "revision": "0b21b8b99b8c52dc6db9",
+    "url": "/firs-react-app/static/js/main.8804bd53.chunk.js"
+  },
+  {
+    "revision": "350b9a24bc05b762fa0a",
+    "url": "/firs-react-app/static/js/runtime-main.8cfcff0f.js"
+  },
+  {
+    "revision": "d836564fb375cc08e741282e7bd2b9ee",
+    "url": "/firs-react-app/static/media/user.d836564f.png"
+  }
+]);
